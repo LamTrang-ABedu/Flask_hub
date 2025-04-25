166386e7e5da11187e9c3eaeeddb99db45a793f5
Lam Trang <lamtrang@abedu.vn>
2025-04-26 01:24:49 +0700

    Add files via upload

--- static/js/media_sources.js
+++ static/js/media_sources.js
@@ -10,17 +10,27 @@ async function loadGallery() {
     grid.innerHTML = `<p style="color:red;">${data.message}</p>`;
     return;
   }
+  const observer = lozad();
   data.results.forEach(item => {
     const card = document.createElement('div');
     card.className = 'media-card';
-    card.innerHTML = `
-  <a href="${item.video}" target="_blank">
-    <img data-src="${item.thumb}" class="lozad" alt="${item.title}" />
-  </a>
-  <p>${item.title}</p>
-`;
+    if (item.type === 'image') {
+      card.innerHTML = `
+        <a href="${item.video}" target="_blank">
+          <img data-src="${item.thumb}" class="lozad" alt="${item.title}" />
+        </a>
+        <p>${item.title}</p>
+      `;
+    } else {
+      card.innerHTML = `
+        <a href="${item.video}" target="_blank">
+          <img data-src="${item.thumb}" class="lozad" alt="${item.title}" />
+        </a>
+        <p>${item.title}</p>
+        <a href="${item.video}" target="_blank" class="watch-btn">Watch Video</a>
+      `;
+    }
     grid.appendChild(card);
   });
-  const observer = lozad('.lozad');
   observer.observe();
 }
