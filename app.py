8a22d3d65eaaa78b9d6f9fb02341b5f4e39f5a26
Lam Trang <lamtrang@abedu.vn>
2025-05-03 21:08:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ def proxy_image():
 
         # Chỉ cho phép proxy ảnh từ các domain hợp lệ
         allowed_domains = {
-            "tranh18x.com": "https://tranh18x.com",
-            "cdn600.tranh18.com": "https://tranh18x.com",
+            "tranh18.com": "https://tranh18.com",
+            "cdn600.tranh18.com": "https://tranh18.com",
             "manhwa18.net": "https://manhwa18.net",
             "static.manhwa18.net": "https://manhwa18.net",
         }
