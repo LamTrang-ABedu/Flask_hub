6a66d4054a586372730d7ab92fe286855ab9630e
lampv <lampv01203@gmail.com>
2025-05-05 22:14:36 +0700

    a

--- utils/media_downloader.py
+++ utils/media_downloader.py
@@ -40,10 +40,6 @@ def download_from_url(url):
 
         elif domain == 'youtube.com':
             ydl_opts.update({
-                'nocheckcertificate': True,
-                'extractor_args': {
-                    'youtubetab': ['skip=authcheck']
-                },
                 'http_headers': {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
                 }
